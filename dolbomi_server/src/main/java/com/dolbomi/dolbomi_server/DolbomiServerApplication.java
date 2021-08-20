2f6fef919b9ddb6d5c0c3e0442ef0ea53edec32b
sieun <tldms201@naver.com>
2021-08-21 01:16:17 +0900

    feat: project init

---
+++ dolbomi_server/src/main/java/com/dolbomi/dolbomi_server/DolbomiServerApplication.java
@@ -0,0 +1,13 @@
+package com.dolbomi.dolbomi_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DolbomiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DolbomiServerApplication.class, args);
+	}
+
+}
---
+++ dolbomi_server/src/test/java/com/dolbomi/dolbomi_server/DolbomiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolbomi.dolbomi_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DolbomiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
